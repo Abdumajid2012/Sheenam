11562f955971d2047a73792a318df826db008d9d
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-11-14 06:13:42 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+// Copyright (c) Coalition Of Good-Hearts Engineers
+// Free To Use To Finfd Comfort And Peace
+//=================================================
+
+using Microsoft.EntityFrameworkCore;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Guest> Guests{get; set;}
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/GenderType.cs
@@ -0,0 +1,14 @@
+﻿//===================================================
+//// Copyright (c) Coalition Of Good-Hearts Engineers
+// Free To Use To Finfd Comfort And Peace
+//===================================================
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Other
+    }
+}
