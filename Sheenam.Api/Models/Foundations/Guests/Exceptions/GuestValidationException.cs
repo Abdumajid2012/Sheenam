dda2f1a886b19a02afffc81f1afd8b3befc837f7
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-11-25 19:47:32 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/GuestValidationException.cs
@@ -0,0 +1,16 @@
+﻿//=================================================
+// Copyright (c) Coalition Of Good-Hearts Engineers
+// Free To Use To Finfd Comfort And Peace
+//=================================================
+
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class GuestValidationException : Xeption
+    {
+        public GuestValidationException() 
+            :base(message: "Guest validaton error oc")
+        {}
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/Exceptions/NullGuestException.cs
@@ -0,0 +1,17 @@
+﻿//=================================================
+// Copyright (c) Coalition Of Good-Hearts Engineers
+// Free To Use To Finfd Comfort And Peace
+//=================================================
+
+using System;
+using Xeptions;
+
+namespace Sheenam.Api.Models.Foundations.Guests.Exceptions
+{
+    public class NullGuestException : Xeption
+    {
+        public NullGuestException() 
+          : base(message:"Guest is null")
+        {}
+    }
+}
